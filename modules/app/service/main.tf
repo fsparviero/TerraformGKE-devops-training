efaa8c8a64ccffb3b6fef619b1f6473dcab39a07
alona19 <76895924+alona19@users.noreply.github.com>
2024-04-23 16:38:51 +0530

    Create main.tf

---
+++ modules/app/service/main.tf
@@ -0,0 +1,19 @@
+
+resource "kubernetes_service" "app_service" {
+  metadata {
+    namespace = var.namespace
+    name      = var.app
+  }
+
+  spec {
+    type = var.lb
+    selector = {
+      app = var.app
+    }
+
+    port {
+      port        = var.port
+      target_port = var.targetPort
+    }
+  }
+}
